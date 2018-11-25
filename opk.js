9e3d816df24cba5c17e966f99fe405a563460df7
James Childress <jchildress@cvent.com>
2018-11-25 14:41:41 -0800

    phrases

--- opk.js
+++ opk.js
@@ -1,28 +1,71 @@
-var lyrics = [
-  { start: 11, end: 13, text: "sippin' tropicana" },
-  { start: 13, end: 16, text: "in a coconut cabana" },
-]
+var screens = [
+  {
+    start: 10,
+    end: 16,
+    lines: [[
+      { start: 11, text: "sip" },
+      { start: 11, text: "pin'" },
+      { text: " " },
+      { start: 12, text: "tro" },
+      { start: 12, text: "pic" },
+      { start: 12, text: "an" },
+      { start: 12, text: "a" },
+    ], [
+      { start: 13, text: "in" },
+      { text: " " },
+      { start: 13, text: "a" },
+      { text: " " },
+      { start: 14, text: "co" },
+      { start: 14, text: "co" },
+      { start: 14, text: "nut" },
+      { text: " " },
+      { start: 15, text: "ca" },
+      { start: 15, text: "ba" },
+      { start: 15, text: "na" },
+    ]]
+  },
+];
+
+var pauses = [
+  { start: 0, end: 10 },
+];
 
 var container = document.getElementById('container');
 
-lyrics.forEach(function(lyric) {
-  node = document.createElement('div');
-  node.innerText = lyric.text;
-  container.appendChild(node);
-  lyric.node = node;
+screens.forEach(function(screen) {
+  div = document.createElement('div');
+  screen.node = div;
+  screen.lines.forEach(function(line) {
+    p = document.createElement('p');
+    div.appendChild(p);
+    line.forEach(function(segment) {
+      span = document.createElement('span');
+      segment.node = span;
+      span.innerText = segment.text;
+      p.appendChild(span);
+    });
+  });
+  container.appendChild(div);
 });
 
 webvfx_add_to_frame = [
-  function(time, browser, frameNumber, frameRate) {
-    lyrics.forEach(function(phrase) {
-      startFrame = phrase.start * frameRate;
-      endFrame = phrase.end * frameRate;
-      if (frameNumber < startFrame || frameNumber > endFrame) {
-        phrase.node.style.opacity = 0;
+  function(time, browser, currentFrame, frameRate) {
+    screens.forEach(function(screen) {
+      var startFrame = screen.start * frameRate;
+      var endFrame = screen.end * frameRate;
+      if (currentFrame < startFrame || currentFrame > endFrame) {
+        screen.node.style.opacity = 0;
       } else {
-        phrase.node.style.opacity = 1;
+        screen.node.style.opacity = 1;
+        screen.lines.forEach(function(line) {
+          line.forEach(function(segment) {
+            var startFrame = segment.start * frameRate;
+            if (startFrame <= currentFrame) {
+              segment.node.classList.add('highlight');
+            }
+          });
+        });
       }
-      phrase.node.innerText = phrase.text + " --- " + startFrame + " " + frameNumber + " " + endFrame;
     });
   }
 ];
