74e5226fba687710b30c85113df9338ed4f6e7ba
Toadslick <to@dslick>
2018-11-28 04:16:11 -0800

    slow audio speed during development

--- opk.js
+++ opk.js
@@ -1,27 +1,29 @@
+var startDelay = 2;
+var endDelay = 0.5;
+
 var screens = [
   {
-    start: 10,
-    end: 16,
+    start: 11.32,
     segments: [
-      { sec: 1.32, text: "sip" },
-      { sec: 0.15, text: "pin'" },
-      { text: " " },
-      { sec: 0.14, text: "tro" },
-      { sec: 0.15, text: "pi" },
-      { sec: 0.17, text: "ca" },
-      { sec: 0.28, text: "na" },
-      { text: "\n"},
-      { sec: 1.30, text: "in" },
-      { text: " " },
-      { sec: 0.16, text: "a" },
-      { text: " " },
-      { sec: 0.16, text: "co" },
-      { sec: 0.16, text: "co" },
-      { sec: 0.14, text: "nut" },
-      { text: " " },
-      { sec: 0.18, text: "ca" },
-      { sec: 0.16, text: "ba" },
-      { sec: 0.30, text: "na" },
+      { text: "sip"  , sec: 0.00 },
+      { text: "pin'" , sec: 0.15 },
+      { text: " "                },
+      { text: "tro"  , sec: 0.14 },
+      { text: "pi"   , sec: 0.15 },
+      { text: "ca"   , sec: 0.17 },
+      { text: "na"   , sec: 0.28 },
+      { text: "\n"               },
+      { text: "in"   , sec: 1.30 },
+      { text: " "    ,           },
+      { text: "a"    , sec: 0.16 },
+      { text: " "                },
+      { text: "co"   , sec: 0.16 },
+      { text: "co"   , sec: 0.16 },
+      { text: "nut"  , sec: 0.14 },
+      { text: " "                },
+      { text: "ca"   , sec: 0.18 },
+      { text: "ba"   , sec: 0.16 },
+      { text: "na"   , sec: 0.30 },
     ]
   },
 ];
@@ -37,13 +39,16 @@ screens.forEach(function(screen) {
   var screenNode = document.createElement('div');
   screenNode.classList.add('kara-screen');
   screen.node = screenNode;
+  var duration = 0;
   screen.segments.forEach(function(segment) {
     var segmentNode = document.createElement('div');
     segmentNode.classList.add('kara-segment');
     segment.node = segmentNode;
     segmentNode.innerText = segment.text;
     screenNode.appendChild(segmentNode);
+    duration += segment.sec || 0;
   });
+  screen.end = screen.start + duration;
   screenContainer.appendChild(screenNode);
 });
 
@@ -61,8 +66,8 @@ pauses.forEach(function(pause) {
 var runLoop = function(time, browser, currentFrame, frameRate) {
 
   screens.forEach(function(screen) {
-    var startFrame = screen.start * frameRate;
-    var endFrame = screen.end * frameRate;
+    var startFrame = (screen.start - startDelay) * frameRate;
+    var endFrame = (screen.end + endDelay) * frameRate;
     if (currentFrame < startFrame || currentFrame > endFrame) {
       screen.node.classList.remove('kara-visible');
     } else {
--- test.js
+++ test.js
@@ -1,7 +1,10 @@
+var speedMultiplier = 0.5;
+
 var runLoop = webvfx_add_to_frame[0];
-var frameRate = 30;
+var frameRate = 30 * speedMultiplier;
 
 var audio = document.getElementById('test-audio');
+audio.playbackRate = speedMultiplier;
 
 var animate = function() {
 	runLoop(null, null, audio.currentTime * frameRate, frameRate);
