7ddd6863b6810dfeffde7b7f06d4bf121697aee6
James Childress <jchildress@cvent.com>
2018-12-03 05:05:46 -0800

    comments for initial dom setup and webvfx effect function

--- video/script.js
+++ video/script.js
@@ -4,6 +4,8 @@ var letterRegex = /[a-z]/;
 var screenContainer = document.getElementById('kara-lyrics-container');
 var pauseContainer = document.getElementById('kara-pause-container');
 
+// Render the `data-animate` JSON for animating the bounce when a segment is highlighted.
+// Webvfx animation start and end times are based on a percentage instead of a timestamp.
 var segmentAnimation = function(startSeconds) {
   var startPercent = startSeconds / totalDuration;
   var endPercent = (startSeconds + segmentAnimationDuration) / totalDuration;
@@ -16,22 +18,42 @@ var segmentAnimation = function(startSeconds) {
   '}';
 };
 
+// Render the nodes for each "screen" of the karaoke song.
+// Each screen consists of many "segments", each of which represent a syllable.
+// Each syllable consists of one or more "letters".
 window.songData.lyrics.forEach(function(screen) {
   var screenNode = document.createElement('div');
   screenNode.classList.add('kara-screen');
   screen.node = screenNode;
-  var duration = 0;
+
+  // Each screen begins at an absolute timestamp, but the timing between segments
+  // is stored as the relative time between them. This is because segments were timed
+  // using a stopwatch.
+  //
+  // While looping through the segments, add their relative duration to the screen's
+  // start time to calculate:
+  // - the screen's end time, when it should be hidden by and its segments ignored.
+  // - the times required for the webvfx data-animate property,
+  //   since webvfx does not support CSS animations.
+  var duration = screen.start;
+
   screen.segments.forEach(function(segment) {
+
+    // Segments without a time property are treated as if they have a duration of zero.
     duration += segment[1] || 0;
 
     var segmentNode = document.createElement('div');
     segmentNode.classList.add('kara-segment');
 
+    // Properties required to be animated by webvfx.
     segmentNode.classList.add('webvfx');
-    segmentNode.setAttribute('data-animate', segmentAnimation(screen.start + duration));
+    segmentNode.setAttribute('data-animate', segmentAnimation(duration));
 
     segment.node = segmentNode;
 
+    // Split each segment into multiple letters.
+    // Only letters, spaces, and line breaks are supported by the
+    // CSS/PNG sprites. (See images/letters.png)
     segment[0].toLowerCase().split('').forEach(function(letter) {
       var letterNode = document.createElement('div');
       if (letterRegex.test(letter)) {
@@ -47,10 +69,12 @@ window.songData.lyrics.forEach(function(screen) {
 
     screenNode.appendChild(segmentNode);
   });
-  screen.end = screen.start + duration;
+  screen.end = duration;
   screenContainer.appendChild(screenNode);
 });
 
+// Render the divs for each "pause". A pause represents a portion of the song
+// when no lyrics are sung, and is denoted by a filling progress meter.
 window.songData.pauses.forEach(function(pause) {
   var pauseNode = document.createElement('div');
   pauseNode.classList.add('kara-pause-meter');
@@ -62,14 +86,23 @@ window.songData.pauses.forEach(function(pause) {
   pauseContainer.appendChild(pauseNode);
 });
 
+// Each webvfx custom effect can basically be treated as a run loop.
+// This function will be called once per video frame by webvfx.
+// For the browser version, requestAnimationFrame calls this function.
 var runLoop = function(time, browser, currentFrame, frameRate) {
 
+  // For each screen, only display the screen if the video's current time
+  // is between the screen's start time and end time.
   window.songData.lyrics.forEach(function(screen) {
     var startFrame = (screen.start - screen.startDelay) * frameRate;
     var endFrame = (screen.end + screen.endDelay) * frameRate;
     if (currentFrame < startFrame || currentFrame > endFrame) {
       screen.node.classList.remove('kara-visible');
     } else {
+
+      // If the screen is visible, loop through each segment and add their
+      // relative times together to get the segment's absolute start time.
+      // Highlight segments whose absolute start time is on or after this frame.
       screen.node.classList.add('kara-visible');
       var segmentStartTime = screen.start;
       screen.segments.forEach(function(segment) {
@@ -84,6 +117,9 @@ var runLoop = function(time, browser, currentFrame, frameRate) {
     }
   });
 
+  // Just like with screens, loop through each pause and
+  // render the bar, as well as it's current progress,
+  // based on the pause's start and end times.
   window.songData.pauses.forEach(function(pause) {
     var startFrame = pause[0] * frameRate;
     var endFrame = pause[1] * frameRate;
@@ -99,4 +135,5 @@ var runLoop = function(time, browser, currentFrame, frameRate) {
   });
 };
 
+// This is where webvfx expects to find custom effect functions.
 window.webvfx_add_to_frame = [runLoop];
